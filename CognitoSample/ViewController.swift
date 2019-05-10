9a80f293bfebf946ae70145f20069ce31cf616e0
Atsushi Nagase <a@ngs.io>
2019-05-10 17:35:40 +0900

    Initial Commit

---
+++ CognitoSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CognitoSample
+//
+//  Created by Atsushi Nagase on 2019/05/10.
+//  Copyright © 2019 instance0, inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
